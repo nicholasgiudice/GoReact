ad2500e038e530ac672348245496c84293791ec8
Nicholas Giudice <nicholas.giudice@outlook.com>
2019-05-06 22:30:56 -0300

    Modulo 2

--- Modulo2/app02/src/App.js
+++ Modulo2/app02/src/App.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 
 import GlobalStyle from './styles/global';
 
-import Main from './pages/Main';
+import Main from './pages/Main/index';
 
 const App = () => (
   <Fragment>
--- Modulo2/app02/src/pages/Main/styles.js
+++ Modulo2/app02/src/pages/Main/styles.js
@@ -1,16 +1,13 @@
-import React from 'react';
 import styled from 'styled-components';
 
-import logo from '../assets/logo.png';
-
-const Container = styled.div`
+export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   padding-top: 60px;
 `;
 
-const Form = styled.form`
+export const Form = styled.form`
   margin-top: 20px;
   width: 100%;
   max-width: 400px;
@@ -20,10 +17,11 @@ const Form = styled.form`
     height: 55px;
     padding: 0 20px;
     background: #fff;
-    border: 0;
     font-size: 18px;
     color: #444;
     border-radius: 3px;
+
+    border: ${props => (props.withError ? '2px solid #F00' : 0)};
   }
 
   button {
@@ -36,21 +34,10 @@ const Form = styled.form`
     font-size: 20px;
     font-weight: bold;
     border-radius: 3px;
+    width: 80px;
 
     &:hover {
       background: #52d89f;
     }
   }
 `;
-
-const Main = () => (
-  <Container>
-    <img src={logo} alt="Github Compare" />
-    <Form>
-      <input type="text" placeholder="Usuário/repositório" />
-      <button type="submit">OK</button>
-    </Form>
-  </Container>
-);
-
-export default Main;
