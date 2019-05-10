bae3351a46f3b9b40960333cc72110848598d890
Nicholas Giudice <nicholas.giudice@outlook.com>
2019-05-09 23:15:48 -0300

    Reactotron

--- Modulo3/app/src/App.js
+++ Modulo3/app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 import Routes from './routes';
+import './config/ReactotronConfig';
 
 function App() {
   return (
